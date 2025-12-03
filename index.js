f44e80d8e1bbb465a3b04683aa19ab720cb06f16
shaggyze <shaggyze@gmail.com>
2025-12-02 21:42:14 -0800

    Update 3.8.5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const fs = require('fs');
 const path = require('path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Options } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({
